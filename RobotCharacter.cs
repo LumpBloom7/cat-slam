50bfe4f5d25c393614cc9e80df8edaddcc798def
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-04-04 18:09:25 +0200

    Add smooth rotation

--- RobotCharacter.cs
+++ RobotCharacter.cs
@@ -3,24 +3,49 @@ using Godot;
 
 public partial class RobotCharacter : CharacterBody3D
 {
+
+    [Export]
+    public float AccelerationPerSecond { get; set; } = 1;
+    [Export]
+    public float DeccelerationPerSecond { get; set; } = 0.5f;
+    [Export]
+    public double TimeUntilStop { get; set; } = 5;
+
+    [Export]
+    public double RotationInterval { get; set; } = 2;
+
+    [Export]
+    public float RotationSpeedRadians { get; set; } = 1;
+
+    [Export]
+    public float MaxSpeed { get; set; } = 1;
+
     private double timeSinceLastRotation = 0;
-    private const float accelerationPerSecond = 1f;
-    private const float decelerationPerSecond = 0.5f;
-    private double timeUntilStop = 5;
+    private float prevRotation = 0;
+    private float newRotation = 0;
+    private double rotationTimeAccumulator = 0;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        prevRotation = newRotation = Rotation.Y;
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
 
         MoveAndSlide();
         timeSinceLastRotation += delta;
+        rotationTimeAccumulator += delta;
 
-        timeUntilStop -= delta;
+        TimeUntilStop -= delta;
 
-        if (timeUntilStop >= 0)
-            Velocity += Vector3.Forward.Rotated(new(0, 1, 0), Rotation.Y) * accelerationPerSecond * (float)delta;
+        if (TimeUntilStop >= 0)
+            Velocity += Vector3.Forward.Rotated(new(0, 1, 0), Rotation.Y) * AccelerationPerSecond * (float)delta;
 
         var noGravityVelocity = Velocity with { Y = 0 };
-        float newSpeed = Math.Min(noGravityVelocity.Length() - decelerationPerSecond * (float)delta, 1);
+        float newSpeed = Math.Min(noGravityVelocity.Length() - DeccelerationPerSecond * (float)delta, MaxSpeed);
 
         if (newSpeed < 0.0001f)
             newSpeed = 0;
@@ -28,10 +53,15 @@ public partial class RobotCharacter : CharacterBody3D
         var norm = noGravityVelocity.Normalized();
 
         Velocity = norm * newSpeed + Vector3.Down;
+        float rotProg = MathF.Min((float)rotationTimeAccumulator / (Math.Abs(newRotation - prevRotation) / RotationSpeedRadians), 1);
 
-        if (timeSinceLastRotation >= 2)
+        Rotation = Rotation with { Y = prevRotation + (newRotation - prevRotation) * rotProg };
+
+        if (timeSinceLastRotation >= RotationInterval)
         {
-            Rotation = Rotation with { Y = (Random.Shared.NextSingle() * 2 - 1) * MathF.PI };
+            prevRotation = Rotation.Y;
+            newRotation = (Random.Shared.NextSingle() * 2 - 1) * MathF.PI;
+            rotationTimeAccumulator = 0;
             timeSinceLastRotation = 0;
         }
     }
