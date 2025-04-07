edb4f741ea7169d6887000ad7d2007cf5d5b438a
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-04-07 11:51:13 +0200

    Use differential motion for robot

--- RobotCharacter.cs
+++ RobotCharacter.cs
@@ -8,61 +8,58 @@ public partial class RobotCharacter : CharacterBody3D
     public float AccelerationPerSecond { get; set; } = 1;
     [Export]
     public float DeccelerationPerSecond { get; set; } = 0.5f;
-    [Export]
-    public double TimeUntilStop { get; set; } = 5;
-
-    [Export]
-    public double RotationInterval { get; set; } = 2;
-
-    [Export]
-    public float RotationSpeedRadians { get; set; } = 1;
 
     [Export]
     public float MaxSpeed { get; set; } = 1;
 
-    private double timeSinceLastRotation = 0;
-    private float prevRotation = 0;
-    private float newRotation = 0;
-    private double rotationTimeAccumulator = 0;
+    [Export]
+    public float Radius { get; set; } = 0.15f;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        prevRotation = newRotation = Rotation.Y;
-    }
+
+    private float leftVel = 0;
+    private float rightVel = 0;
 
     public override void _PhysicsProcess(double delta)
     {
-        base._PhysicsProcess(delta);
+        // Compute acceleration amounts
+        float leftAcc = 0;
+        float rightAcc = 0;
+        if (Input.IsActionPressed("LeftMotorForwards"))
+            leftAcc += AccelerationPerSecond * (float)delta;
+        if (Input.IsActionPressed("LeftMotorBackwards"))
+            leftAcc -= AccelerationPerSecond * (float)delta;
+
+        if (Input.IsActionPressed("RightMotorForwards"))
+            rightAcc += AccelerationPerSecond * (float)delta;
+        if (Input.IsActionPressed("RightMotorBackwards"))
+            rightAcc -= AccelerationPerSecond * (float)delta;
+
+        // Apply speed changes
+        if (leftAcc == 0)
+            leftVel = Math.Max(Math.Abs(leftVel) - DeccelerationPerSecond * (float)delta, 0) * MathF.Sign(leftVel);
+        else
+            leftVel = Math.Clamp(leftAcc + leftVel, -MaxSpeed, MaxSpeed);
+
+        if (rightAcc == 0)
+            rightVel = Math.Max(Math.Abs(rightVel) - DeccelerationPerSecond * (float)delta, 0) * MathF.Sign(rightVel);
+        else
+            rightVel = Math.Clamp(rightAcc + rightVel, -MaxSpeed, MaxSpeed);
+
+        float rotAmount = (rightVel - leftVel) / (Radius * 2);
+
+
+        Console.WriteLine(AccelerationPerSecond);
+        Console.WriteLine(leftAcc);
+        Console.WriteLine(rightAcc);
+
+        Rotation = Rotation with { Y = Rotation.Y + rotAmount * (float)delta };
+
+        float velocity = (leftVel + rightVel) / 2;
+
+        Vector3 movementVector = (Vector3.Forward * velocity).Rotated(new Vector3(0, 1, 0), Rotation.Y);
+
+        Velocity = movementVector;
 
         MoveAndSlide();
-        timeSinceLastRotation += delta;
-        rotationTimeAccumulator += delta;
-
-        TimeUntilStop -= delta;
-
-        if (TimeUntilStop >= 0)
-            Velocity += Vector3.Forward.Rotated(new(0, 1, 0), Rotation.Y) * AccelerationPerSecond * (float)delta;
-
-        var noGravityVelocity = Velocity with { Y = 0 };
-        float newSpeed = Math.Min(noGravityVelocity.Length() - DeccelerationPerSecond * (float)delta, MaxSpeed);
-
-        if (newSpeed < 0.0001f)
-            newSpeed = 0;
-
-        var norm = noGravityVelocity.Normalized();
-
-        Velocity = norm * newSpeed + Vector3.Down;
-        float rotProg = MathF.Min((float)rotationTimeAccumulator / (Math.Abs(newRotation - prevRotation) / RotationSpeedRadians), 1);
-
-        Rotation = Rotation with { Y = prevRotation + (newRotation - prevRotation) * rotProg };
-
-        if (timeSinceLastRotation >= RotationInterval)
-        {
-            prevRotation = Rotation.Y;
-            newRotation = (Random.Shared.NextSingle() * 2 - 1) * MathF.PI;
-            rotationTimeAccumulator = 0;
-            timeSinceLastRotation = 0;
-        }
     }
 }
