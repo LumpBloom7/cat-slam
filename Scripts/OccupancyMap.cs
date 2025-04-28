d76f116c56b0c313ba4e65ea4d7966d42cb03193
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-04-28 15:46:12 +0200

    Use proper triangulation without bearing

---
+++ Scripts/OccupancyMap.cs
@@ -0,0 +1,22 @@
+/* using System.Collections;
+using Godot;
+
+public partial class OccupancyMap : GridMap
+{
+
+    public IEnumerable getIntersectingTiles(Vector3 origin, Vector3 target)
+    {
+        const prec
+        Cell cell = new Cell { X = -5000, Y = -5000 };
+
+
+    }
+
+    private readonly record struct Cell
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+
+    }
+}
+ */
--- Scripts/RobotCharacter.cs
+++ Scripts/RobotCharacter.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Godot;
 
 public partial class RobotCharacter : CharacterBody3D
@@ -121,39 +123,88 @@ public partial class RobotCharacter : CharacterBody3D
         //GD.Print(kalmanFilter?.Sigma);
     }
 
+    private Vector2? triangulate(List<(System.Numerics.Vector2 Position, float Distance)> trackedBeacons)
+    {
+        var offset = trackedBeacons[0].Position;
+        var p2 = trackedBeacons[1].Position - offset;
+        p2.Y *= -1;
+        var p3 = trackedBeacons[2].Position - offset;
+        p3.Y *= -1;
+
+        float theata = MathF.Acos(p2.X / p2.Length());
+        (float sin, float cos) = MathF.SinCos(-theata);
+
+        var np2 = new Vector2(p2.X * cos - p2.Y * sin, p2.X * sin + p2.Y * cos);
+        var np3 = new Vector2(p3.X * cos - p3.Y * sin, p3.X * sin + p3.Y * cos);
+
+        float r1 = trackedBeacons[0].Distance;
+        float r2 = trackedBeacons[1].Distance;
+        float r3 = trackedBeacons[2].Distance;
+
+        float p4x = (r1 * r1 - r2 * r2 + np2.X * np2.X) / (2 * np2.X);
+        float p4y = (r1 * r1 - r3 * r3 + np3.X * np3.X + np3.Y * np3.Y) / (2 * np3.Y) - (np3.X / np3.Y) * p4x;
+
+        if (!float.IsFinite(p4y))
+        {
+            Console.WriteLine($"Triangulation Y is NaN, ignoring attempt");
+            return null;
+        }
+
+        if (!float.IsFinite(p4x))
+        {
+            Console.WriteLine($"Triangulation X is NaN, ignoring attempt");
+            return null;
+        }
+
+        (float cSin, float cCos) = MathF.SinCos(theata);
+
+        Vector2 p4 = new(p4x * cCos - p4y * cSin, p4x * cSin + p4y * cCos);
+
+        return new(p4.X + offset.X, -p4.Y + offset.Y);
+    }
+
+    // Iterative, using 'i' as bitmask to choose each combo members
+    public static List<List<T>> GetAllCombos<T>(List<T> list)
+    {
+        int comboCount = (int)Math.Pow(2, list.Count) - 1;
+        List<List<T>> result = new List<List<T>>();
+        for (int i = 1; i < comboCount + 1; i++)
+        {
+            // make each combo here
+            result.Add(new List<T>());
+            for (int j = 0; j < list.Count; j++)
+            {
+                if ((i >> j) % 2 != 0)
+                    result.Last().Add(list[j]);
+            }
+        }
+        return result;
+    }
+
     private void updateKalmanFilter(float omega, float vel, double dt, float theta)
     {
         var u = MathNet.Numerics.LinearAlgebra.Single.Vector.Build.Dense([vel, omega]);
 
-        Vector2 sum = Vector2.Zero;
-        int count = 0;
-        foreach (var sensor in BeaconDetector.GetTrackedBeacons())
+        var combinations = GetAllCombos(BeaconDetector.GetTrackedBeacons().ToList()).Where(c => c.Count == 3);
+
+        var items = combinations.Select(k => triangulate([.. k])).Where(i => i.HasValue).Select(i => i!.Value).ToArray();
+
+        if (items.Length > 0)
         {
-            //GD.Print($"SensorX: {sensor.Position.X}");
-            //GD.Print($"SensorY: {sensor.Position.Y}");
+            Vector2 avgPos = new();
+            foreach (var i in items)
+                avgPos += i;
 
-            // We flip the sign of Y
-            float bearing = MathF.Atan2(-(sensor.Position.Y - Position.Z), sensor.Position.X - Position.X);
+            avgPos /= items.Length;
 
-            (float sin, float cos) = MathF.SinCos(bearing);
-
-            Vector2 estimatedPosition = new Vector2(-sensor.Distance * cos, sensor.Distance * sin) + new Vector2(sensor.Position.X, sensor.Position.Y);
-            sum += estimatedPosition;
-            ++count;
-        }
-
-        Vector2 avgPos = sum / Math.Max(1, count);
-
-        //GD.Print($"Average position: {avgPos}");
-        if (count == 0)
-        {
-            kalmanFilter?.Update(u, null, dt);
-        }
-        else
-        {
             var z = MathNet.Numerics.LinearAlgebra.Single.Vector.Build.Dense([avgPos.X, -avgPos.Y, theta.ToMathematicalAngle()]);
             kalmanFilter?.Update(u, z, dt);
         }
+        else
+        {
+            kalmanFilter?.Update(u, null, dt);
+        }
+
         Debug.Assert(kalmanFilter is not null);
         ghostNode.Position = Position with { X = kalmanFilter.Mu[0], Z = -kalmanFilter.Mu[1] };
         ghostNode.Rotation = new Vector3(0, kalmanFilter.Mu[2].FromMathematicalAngle(), 0);
