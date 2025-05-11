11a4ec684bdf1a7a19d67e0d0ec4847936b7c88b
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-05-11 15:57:56 +0200

    Make tf work

--- Scripts/Evolutionary Roborics/NeuralNetwork.cs
+++ Scripts/Evolutionary Roborics/NeuralNetwork.cs
@@ -1,21 +1,37 @@
 using System;
-using static Tensorflow.Binding;
-
 using Tensorflow;
 using Tensorflow.Keras;
-using Tensorflow.Keras.Models;
 using Tensorflow.Keras.Layers;
+using Tensorflow.Keras.Models;
+using static Tensorflow.Binding;
 
 public class Network
 {
     public static void Main(string[] args)
     {
-        var model = new tf.model.Sequential();
-        model.Add(new Dense(12, activation: "relu", input_dim: 5));
-        model.Add(new Dense(8, activation: "relu"));
-        model.Add(new Dense(3, activation: "softmax"));
+        var activations = new Activations();
+        var model = new Tensorflow.Keras.Engine.Sequential(
+            new Tensorflow.Keras.ArgsDefinition.SequentialArgs
+            {
+                Layers = [
+                    new Dense(new Tensorflow.Keras.ArgsDefinition.DenseArgs{
+                        Units = 12,
+                        Activation =  activations.Relu,
+                        InputShape = new Shape(5,5)
+                    }),
+                    new Dense(new Tensorflow.Keras.ArgsDefinition.DenseArgs{
+                        Units = 8,
+                        Activation =  activations.Relu
+                    }),
+                    new Dense(new Tensorflow.Keras.ArgsDefinition.DenseArgs{
+                        Units = 3,
+                        Activation =  activations.Softmax,
+                    })
+                ]
+            }
+        );
 
-        model.compile(optimizer: "adam", loss: "mean_squared_error", metrics: new[] { "accuracy" });
+        model.compile(optimizer: "adam", loss: "mean_squared_error", metrics: ["accuracy"]);
 
         Console.WriteLine("Model Built.");
     }
