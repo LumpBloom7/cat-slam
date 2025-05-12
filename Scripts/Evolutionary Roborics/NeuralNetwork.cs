9acaffb20d426029d9f6be91bd18d54aa7d2873e
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-05-12 17:02:26 +0200

    To torchsharp we go

--- Scripts/Evolutionary Roborics/NeuralNetwork.cs
+++ Scripts/Evolutionary Roborics/NeuralNetwork.cs
@@ -1,37 +1,34 @@
 using System;
-using Tensorflow;
-using Tensorflow.Keras;
-using Tensorflow.Keras.Layers;
-using Tensorflow.Keras.Models;
-using static Tensorflow.Binding;
+
+using TorchSharp;
+using static TorchSharp.torch.nn;
 
 public class Network
 {
     public static void Main(string[] args)
     {
-        var activations = new Activations();
-        var model = new Tensorflow.Keras.Engine.Sequential(
-            new Tensorflow.Keras.ArgsDefinition.SequentialArgs
-            {
-                Layers = [
-                    new Dense(new Tensorflow.Keras.ArgsDefinition.DenseArgs{
-                        Units = 12,
-                        Activation =  activations.Relu,
-                        InputShape = new Shape(5,5)
-                    }),
-                    new Dense(new Tensorflow.Keras.ArgsDefinition.DenseArgs{
-                        Units = 8,
-                        Activation =  activations.Relu
-                    }),
-                    new Dense(new Tensorflow.Keras.ArgsDefinition.DenseArgs{
-                        Units = 3,
-                        Activation =  activations.Softmax,
-                    })
-                ]
-            }
-        );
 
-        model.compile(optimizer: "adam", loss: "mean_squared_error", metrics: ["accuracy"]);
+        var lin1 = Linear(1000, 100);
+        var lin2 = Linear(100, 10);
+        var seq = Sequential(("lin1", lin1), ("relu1", ReLU()), ("drop1", Dropout(0.1)), ("lin2", lin2));
+
+        using var x = torch.randn(64, 1000);
+        using var y = torch.randn(64, 10);
+
+        var optimizer = torch.optim.Adam(seq.parameters());
+
+        for (int i = 0; i < 10; i++)
+        {
+            using var eval = seq.forward(x);
+            using var output = functional.mse_loss(eval, y, Reduction.Sum);
+
+            optimizer.zero_grad();
+
+            output.backward();
+
+            optimizer.step();
+        }
+
 
         Console.WriteLine("Model Built.");
     }
