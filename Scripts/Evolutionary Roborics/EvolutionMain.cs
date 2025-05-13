21eadccbcdebe3a5ced67c56a3e36f8932723a1c
Nataix24 <czbn24@gmail.com>
2025-05-13 20:58:09 +0200

    Pray for us

--- Scripts/Evolutionary Roborics/EvolutionMain.cs
+++ Scripts/Evolutionary Roborics/EvolutionMain.cs
@@ -1,55 +1,55 @@
-using MyProject.Algorithms;
-using MyProject.NeuralNetwork;
-namespace MyProject;
 
 using System;
+using System.Data;
 
-class Program
+public class EvolutionMain
 {
-    static void Main(string[] args)
-    {
+    public Model model;
+    private int k; //5
+    private int populationSize; //10
+    private float parentSelectionPersentage; //0.20f;
+    private float mutationRate; //0.02f
+    private Genome bestGenome;
+    SimulationProvider.SimulationContext simulation;
 
-        float[] sensorInput = [3.0f, 4.0f, 1.0f, 3.0f];
-        float[] motorVelocity = [0.2f, 1.4f];
-
+    public EvolutionMain(int populationSize, int k, int parentSelectionPersentage, int mutationRate){
+        this.k = k;
+        this.populationSize = populationSize;
+        this.parentSelectionPersentage = parentSelectionPersentage;
+        this.mutationRate = mutationRate;
+    }
+    public (int,int) update(SimulationProvider.SimulationContext simulation){
         //Initialize Neural Network
-        Model model = new Model(sensorInput.Length, motorVelocity.Length);
-
-        // Network model = new Network;
-        // model.run();
-
-        int k = 5;
-        int populationSize = 100;
-        Console.Write("weights" + model.countNeurons());
-        //int populationSize = 10;
+        int sensorLength = simulation.SensorValues.Length;
+        int motorLength = 2;
+        model = new Model(sensorLength + motorLength, motorLength);
         int numOfWeights = model.countNeurons(); // num of weights of NN determines the size of our Genomes
-        float parentSelectionPersentage = 0.20f;
-        float mutationRate = 0.02f;
-        //     GeneticAlgorithm ga = new GeneticAlgorithm(populationSize, numOfWeights, k, parentSelectionPersentage, mutationRate);
 
-        //     //Get the population and print genomes
+        GeneticAlgorithm ga = new GeneticAlgorithm(populationSize, numOfWeights, k, parentSelectionPersentage, mutationRate, model, simulation);
+        //Run the Genetic Algorithm
+        int count = 1;
+        float generationBestFintess = 1000000000;
+        int hasGenerationImproved = 0;
 
-        //     // Run the Genetic Algorithm
-        //     int count = 1;
-        //     while (count != 4)
-        //     {
-        //         Console.WriteLine("--------------");
-        //         Console.WriteLine("round: " + count);
-        //         var population = ga.population.Genomes;
-        //         for (int i = 0; i < population.Length; i++)
-        //         {
-        //             Console.WriteLine($"Genome {i + 1}:");
-        //             float[] weights = population[i].weights;
-        //             foreach (var weight in weights)
-        //             {
-        //                 Console.WriteLine(weight);
-        //             }
-        //         }
-
-        //         ga.Run();
-        //         Console.WriteLine("Populatioin: " + count + "best fit: " + ga.bestFitness);
-        //         count++;
-        //     }
-
+        while (count != 4 | hasGenerationImproved < 10)
+        {
+            //Console.WriteLine("--------------");
+            //Console.WriteLine("round: " + count);
+            var population = ga.population.Genomes;
+            ga.Run();
+            if (generationBestFintess == 1000000000 || ga.bestGenome.FitnessScore < generationBestFintess)
+            {
+                generationBestFintess = ga.bestGenome.FitnessScore;
+                hasGenerationImproved = 0;
+                bestGenome = ga.bestGenome;
+            }
+            else
+            {
+                hasGenerationImproved += 1;
+            }
+            //re evaluate shild with best fitness and get motor left right etc.
+        }
+        (int,int) action = ga.evaluateToGetAction(bestGenome);
+        return action;
     }
 }
--- Scripts/Evolutionary Roborics/GeneticAlgorithm.cs
+++ Scripts/Evolutionary Roborics/GeneticAlgorithm.cs
@@ -2,256 +2,257 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using MyProject.NeuralNetwork;
 using TorchSharp;
 
-namespace MyProject.Algorithms
+public class GeneticAlgorithm
 {
-    public class GeneticAlgorithm
+    private int k;
+    public Population population;
+    private float parentSelectionPersentage;
+    private int genomeSize;
+    float mutationRate;
+    private Model model;
+    public Genome bestGenome;
+    SimulationProvider.SimulationContext simulation;
+
+    public GeneticAlgorithm(int populationSize, int genomeSize, int torunamentSeelectionSize, float parentSelectionPersentage, float mutationRate, Model model,SimulationProvider.SimulationContext simulation)
     {
-        private int k;
-        public Population population;
-        private float parentSelectionPersentage;
-        private int genomeSize;
-        float mutationRate;
-        public float bestFitness;
-
-        public GeneticAlgorithm(int populationSize, int genomeSize, int torunamentSeelectionSize, float parentSelectionPersentage, float mutationRate)
+        this.mutationRate = mutationRate;
+        this.genomeSize = genomeSize;
+        k = torunamentSeelectionSize;
+        this.parentSelectionPersentage = parentSelectionPersentage;
+        population = new Population(populationSize, genomeSize);
+        var populationEvaluate = population.Genomes;
+        this.model = model;
+        this.simulation=simulation;
+        for (int i = 0; i < populationEvaluate.Length; i++)
         {
-            this.mutationRate = mutationRate;
-            this.genomeSize = genomeSize;
-            k = torunamentSeelectionSize;
-            this.parentSelectionPersentage = parentSelectionPersentage;
-            population = new Population(populationSize, genomeSize);
-            var populationEvaluate = population.Genomes;
-            for (int i = 0; i < populationEvaluate.Length; i++)
-            {
-                //populationEvaluate[i].evaluateFitness();
-                populationEvaluate[i].FitnessScore = evaluate(populationEvaluate[i]);
-                float fitness = populationEvaluate[i].FitnessScore;
-                //Console.WriteLine(fitness);
-            }
-        }
-
-        private float evaluate(Genome individual)
-        { // accepts an individual (We will simulate his game)
-            //Here we get sensor inputs and motor velocities for input + restart the enviroment
-            float[] sensorInput = [3.0f, 4.0f, 1.0f, 3.0f];
-            float[] motorVelocity = [0.2f, 1.4f];
-            //Define the NN
-            Model model = new Model(sensorInput.Length, motorVelocity.Length);
-            model.setWeights(individual.weights); // assign the weights to out NN
-            int step = 0;
-            int award = 0;
-
-            while (step < 1000)
-            {
-                // here we get our observations but i am unsure how to retrieve them from godot dynamically so i am using place holder
-                var seq = model.seq;
-                var x = torch.tensor(sensorInput); // forgot we need to include sensor Input+ motor inputs
-                var y = torch.tensor(new float[] { 2f, 3f });
-                //perform step
-                using var action = seq.forward(x);
-                //Now that we have an action we perform a move in out enviroment from which we get award (like hitting wall -200 or smth, collecting a cat +1000)
-                // reward, observation = envitoment.step()
-                //award += reward
-                step = step + 1;
-                //sensorInput = obs2 set to new sensor readings
-            }
-            return 1f; // placeholder
-        }
-        private Genome tournamentSelection()
-        {
-            ///Console.WriteLine("--------------------------");
-            //Console.WriteLine("Tournament Selection");
-            Debug.Assert(population.Genomes.Length > 0);
-
-            Random.Shared.Shuffle(population.Genomes);
-
-            return population.Genomes.Take(k).MinBy(g => g.FitnessScore)!;
-        }
-
-        private Genome[] selectParents()
-        {
-            // Calculate 10% of the population size
-            int numberOfParents = (int)(population.PopulationSize * parentSelectionPersentage);
-            if (numberOfParents < 2)
-                return [];
-
-            HashSet<Genome> selectedParents = [];
-
-            while (selectedParents.Count < numberOfParents)
-                selectedParents.Add(tournamentSelection());
-
-            return [.. selectedParents];
-        }
-
-        public float[] singlePointCrossover(float[] parent1, float[] parent2)
-        {
-            //Console.WriteLine("single crossover");
-            int length = parent1.Length;
-            Random rand = Random.Shared;
-            int crossoverPoint = rand.Next(1, length); //not 0 or length
-
-            //Randomly choose which parent contributes the first half
-            bool firstHalfFromParent1 = rand.NextDouble() < 0.5;
-            float[] child = new float[length];
-
-            for (int i = 0; i < length; i++)
-            {
-                if (i < crossoverPoint)
-                    child[i] = firstHalfFromParent1 ? parent1[i] : parent2[i];
-                else
-                    child[i] = firstHalfFromParent1 ? parent2[i] : parent1[i];
-            }
-
-            return child;
-        }
-
-        public float[] mutate(float mutationRate, float[] childWeights)
-        {
-            Random random = Random.Shared;
-
-            //Console.Write("in mutation");
-            if (random.NextDouble() < mutationRate)
-            {
-                Console.WriteLine("Mutation performed");
-                int index = random.Next(childWeights.Length);
-                childWeights[index] += (float)(random.NextDouble() * 2 - 1); //Random between -1 and 1
-            }
-            return childWeights;
-        }
-
-
-        public void Run()
-        {
-            Console.WriteLine("Running Genetic Algorithm..."); //GA process: selection, crossover, mutation, etc.
-            //Console.WriteLine("");
-
-            //select parents
-            var parents = selectParents();
-            //Console.WriteLine("Parent length: " + parents.Length);
-
-            // Create new population, include parents implicitly
-            List<Genome> newPopulation = [.. parents];
-
-            Random rand = Random.Shared;
-
-            while (newPopulation.Count < population.PopulationSize)
-            {
-                //Console.WriteLine("population creation count: "+count);
-                //Select two random parents
-                Genome parent1 = parents[rand.Next(parents.Length)];
-                Genome parent2 = parents[rand.Next(parents.Length)];
-                //Avoid selecting same parent
-                while (parent1 == parent2)
-                    parent2 = parents[rand.Next(parents.Length)];
-
-                //Perform crossover
-                float[] childWeights = singlePointCrossover(parent1.weights, parent2.weights);
-
-                //Perform mutation
-                childWeights = mutate(mutationRate: mutationRate, childWeights);
-
-                //Create new genome
-                Genome childGenome = new Genome(childWeights);
-                childGenome.FitnessScore = evaluate(childGenome);
-                //childGenome.evaluateFitness();
-                newPopulation.Add(childGenome);
-            }
-
-            //Store best fit
-            float bestFitness = 10000000;
-            for (int i = 0; i < newPopulation.Count; i++)
-            {
-                Console.WriteLine("parent " + i + "fitness: " + newPopulation[i].FitnessScore);
-                float fitness = newPopulation[i].FitnessScore;
-                if (bestFitness == 10000000 || fitness < bestFitness)
-                {
-                    bestFitness = fitness;
-                }
-            }
-            this.bestFitness = bestFitness;
-
-            Population newGeneration = new Population(genomeSize, [.. newPopulation]);
-            population = newGeneration;
+            //populationEvaluate[i].evaluateFitness();
+            populationEvaluate[i].FitnessScore = evaluate(populationEvaluate[i]);
+            //float fitness = populationEvaluate[i].FitnessScore;
+            //Console.WriteLine(fitness);
         }
     }
 
-    public class Genome
-    {
-        public float FitnessScore { get; set; }
-        public float[] weights { get; set; } // At start initialize weights (they are equal to the amount of neurons in NN)
-        public Genome(int numWeights) //Initialize random weights
+    private float evaluate(Genome individual)
+    { // accepts an individual (We will simulate his game)
+      //Here we get sensor inputs and motor velocities for input + restart the enviroment
+        simulation.Reset();
+
+        float[] sensorInput = simulation.SensorValues;
+        float[] motorVelocity = [simulation.LeftVel, simulation.RightVel];
+        float[] neuralNetworkInput = new float[sensorInput.Length + motorVelocity.Length];
+        sensorInput.CopyTo(neuralNetworkInput, 0);
+        motorVelocity.CopyTo(neuralNetworkInput, sensorInput.Length);
+
+        //Define the NN
+        model.setWeights(individual.weights); // assign the weights to out NN
+        int step = 0;
+        int award = 0;
+
+        while (step < 1000)
         {
-            //Initialize weights with random values between -1 and 1
-            Random random = new Random();
-            weights = new float[numWeights];
-            for (int i = 0; i < numWeights; i++)
+
+            //here we get our observations but i am unsure how to retrieve them from godot dynamically so i am using place holder
+            var seq = model.seq;
+            var eval = torch.tensor(neuralNetworkInput);
+            //perform step
+            using var action = seq.forward(eval);
+            simulation.Update((int)Math.Round(eval.data<float>()[0]), (int)Math.Round(eval.data<float>()[1])); //update enviroment
+            award = simulation.ComputeReward();
+            //update neural network inputs
+            simulation.SensorValues.CopyTo(neuralNetworkInput, 0);
+            motorVelocity[0] = simulation.LeftVel;
+            motorVelocity[1] = simulation.RightVel;
+            motorVelocity.CopyTo(neuralNetworkInput, sensorInput.Length);
+            step = step + 1;
+        }
+        return 1f; // placeholder
+    }
+    public (int, int) evaluateToGetAction(Genome individual)
+    { 
+        simulation.Reset();
+
+        float[] sensorInput = simulation.SensorValues;
+        float[] motorVelocity = [simulation.LeftVel, simulation.RightVel];
+        float[] neuralNetworkInput = new float[sensorInput.Length + motorVelocity.Length];
+        sensorInput.CopyTo(neuralNetworkInput, 0);
+        motorVelocity.CopyTo(neuralNetworkInput, sensorInput.Length);
+
+        model.setWeights(individual.weights); // assign the weights to out NN
+        var seq = model.seq;
+        var x = torch.tensor(neuralNetworkInput); // forgot we need to include sensor Input+ motor inputs
+        using var action = seq.forward(x);
+        int leftMotorReading = (int)Math.Round(x.data<float>()[0]);
+        int rightMotorReading = (int)Math.Round(x.data<float>()[1]);
+        return (leftMotorReading, rightMotorReading); // placeholder
+    }
+    private Genome tournamentSelection()
+    {
+        ///Console.WriteLine("--------------------------");
+        //Console.WriteLine("Tournament Selection");
+        Debug.Assert(population.Genomes.Length > 0);
+
+        Random.Shared.Shuffle(population.Genomes);
+
+        return population.Genomes.Take(k).MinBy(g => g.FitnessScore)!;
+    }
+
+    private Genome[] selectParents()
+    {
+        // Calculate 10% of the population size
+        int numberOfParents = (int)(population.PopulationSize * parentSelectionPersentage);
+        if (numberOfParents < 2)
+            return [];
+
+        HashSet<Genome> selectedParents = [];
+
+        while (selectedParents.Count < numberOfParents)
+            selectedParents.Add(tournamentSelection());
+
+        return [.. selectedParents];
+    }
+
+    public float[] singlePointCrossover(float[] parent1, float[] parent2)
+    {
+        //Console.WriteLine("single crossover");
+        int length = parent1.Length;
+        Random rand = Random.Shared;
+        int crossoverPoint = rand.Next(1, length); //not 0 or length
+
+        //Randomly choose which parent contributes the first half
+        bool firstHalfFromParent1 = rand.NextDouble() < 0.5;
+        float[] child = new float[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            if (i < crossoverPoint)
+                child[i] = firstHalfFromParent1 ? parent1[i] : parent2[i];
+            else
+                child[i] = firstHalfFromParent1 ? parent2[i] : parent1[i];
+        }
+
+        return child;
+    }
+
+    public float[] mutate(float mutationRate, float[] childWeights)
+    {
+        Random random = Random.Shared;
+
+        //Console.Write("in mutation");
+        if (random.NextDouble() < mutationRate)
+        {
+            Console.WriteLine("Mutation performed");
+            int index = random.Next(childWeights.Length);
+            childWeights[index] += (float)(random.NextDouble() * 2 - 1); //Random between -1 and 1
+        }
+        return childWeights;
+    }
+
+
+    public void Run()
+    {
+        Console.WriteLine("Running Genetic Algorithm..."); //GA process: selection, crossover, mutation, etc.
+                                                           //Console.WriteLine("");
+
+        //select parents
+        var parents = selectParents();
+        //Console.WriteLine("Parent length: " + parents.Length);
+
+        // Create new population, include parents implicitly
+        List<Genome> newPopulation = [.. parents];
+
+        Random rand = Random.Shared;
+
+        while (newPopulation.Count < population.PopulationSize)
+        {
+            //Console.WriteLine("population creation count: "+count);
+            //Select two random parents
+            Genome parent1 = parents[rand.Next(parents.Length)];
+            Genome parent2 = parents[rand.Next(parents.Length)];
+            //Avoid selecting same parent
+            while (parent1 == parent2)
+                parent2 = parents[rand.Next(parents.Length)];
+
+            //Perform crossover
+            float[] childWeights = singlePointCrossover(parent1.weights, parent2.weights);
+
+            //Perform mutation
+            childWeights = mutate(mutationRate: mutationRate, childWeights);
+
+            //Create new genome
+            Genome childGenome = new Genome(childWeights);
+            childGenome.FitnessScore = evaluate(childGenome);
+            //childGenome.evaluateFitness();
+            newPopulation.Add(childGenome);
+        }
+
+        //Store best fit
+        float bestFitness = 10000000;
+        for (int i = 0; i < newPopulation.Count; i++)
+        {
+            Console.WriteLine("parent " + i + "fitness: " + newPopulation[i].FitnessScore);
+            float fitness = newPopulation[i].FitnessScore;
+            if (bestFitness == 10000000 || fitness < bestFitness)
             {
-                weights[i] = (float)(random.NextDouble() * 2 - 1); // Random between -1 and 1
+                bestGenome = newPopulation[i];
             }
         }
 
-        public Genome(float[] weights) //Initialize with predefined weights
-        {
-            this.weights = weights;
-        }
-
-        // public void evaluateFitness()
-        // {
-        //     float errorSum = 0;
-        //     int numSamples = 100; // Number of samples to test the polynomial on
-        //     float xMin = -10f; // Minimum x value for testing
-        //     float xMax = 10f;  // Maximum x value for testing
-        //     float step = (xMax - xMin) / numSamples; // Step size to sample values of x
-
-        //     // Iterate over x values to calculate the error (MSE)
-        //     for (float x = xMin; x <= xMax; x += step)
-        //     {
-        //         // Genome represents the polynomial coefficients: a, b, c (for a*x^2 + b*x + c)
-        //         float polynomialValue = this.weights[0] * x * x + this.weights[1] * x + this.weights[2]; // ax^2 + bx + c
-        //         float targetValue = TargetPolynomial(x); // Target polynomial value
-
-        //         // Compute the squared error between the genome's polynomial and the target polynomial
-        //         errorSum += (polynomialValue - targetValue) * (polynomialValue - targetValue);
-        //     }
-
-        //     // Return the mean squared error (MSE)
-        //     this.FitnessScore = errorSum / numSamples;
-        // }
-
-        public static float TargetPolynomial(float x) => x * x + 3 * x + 2;
-
-    }
-
-    public class Population
-    {
-        public Genome[] Genomes { get; set; } = [];
-        public int PopulationSize => Genomes.Length;
-        private int genomeSize;
-
-        public Population(int populationSize, int genomeSize)
-        {
-            Console.WriteLine("Random Population initialization");
-            this.genomeSize = genomeSize;
-            InitializePopulation(populationSize);
-        }
-        public Population(int genomeSize, Genome[] population)
-        {
-            Console.WriteLine("new Generation initialization");
-            this.genomeSize = genomeSize;
-            Genomes = population;
-        }
-
-        private void InitializePopulation(int populationSize)
-        {
-            Genome[] genomes = new Genome[populationSize];
-
-            for (int i = 0; i < populationSize; i++)
-                genomes[i] = new Genome(genomeSize); // Initialize each genome with random weights
-
-            Genomes = genomes;
-        }
+        Population newGeneration = new Population(genomeSize, [.. newPopulation]);
+        population = newGeneration;
     }
 }
+
+public class Genome
+{
+    public float FitnessScore { get; set; }
+    public float[] weights { get; set; } // At start initialize weights (they are equal to the amount of neurons in NN)
+    public Genome(int numWeights) //Initialize random weights
+    {
+        //Initialize weights with random values between -1 and 1
+        Random random = new Random();
+        weights = new float[numWeights];
+        for (int i = 0; i < numWeights; i++)
+        {
+            weights[i] = (float)(random.NextDouble() * 2 - 1); // Random between -1 and 1
+        }
+    }
+
+    public Genome(float[] weights) //Initialize with predefined weights
+    {
+        this.weights = weights;
+    }
+
+}
+
+public class Population
+{
+    public Genome[] Genomes { get; set; } = [];
+    public int PopulationSize => Genomes.Length;
+    private int genomeSize;
+
+    public Population(int populationSize, int genomeSize)
+    {
+        Console.WriteLine("Random Population initialization");
+        this.genomeSize = genomeSize;
+        InitializePopulation(populationSize);
+    }
+    public Population(int genomeSize, Genome[] population)
+    {
+        Console.WriteLine("new Generation initialization");
+        this.genomeSize = genomeSize;
+        Genomes = population;
+    }
+
+    private void InitializePopulation(int populationSize)
+    {
+        Genome[] genomes = new Genome[populationSize];
+
+        for (int i = 0; i < populationSize; i++)
+            genomes[i] = new Genome(genomeSize); // Initialize each genome with random weights
+
+        Genomes = genomes;
+    }
+}
+
