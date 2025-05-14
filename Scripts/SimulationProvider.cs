5c345a4679cbac79ffca0b84f1bd2d0559836cad
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-05-14 21:48:32 +0200

    Minor fix to simulation *movement*
    STEPSIZE not accounted for

--- Scripts/SimulationProvider.cs
+++ Scripts/SimulationProvider.cs
@@ -74,22 +74,17 @@ public partial class SimulationProvider : Node
         public SimulationContext Copy() => new(this);
 
         private readonly int numSensor;
-
         private readonly OccupancyMap occupancyMap;
         private readonly Vector3 originalPosition;
         private readonly float originalRotation;
         private readonly float[] originalDistances;
-
-        private float AccelerationPerSecond { get; set; } = 1;
-
-        private float DeccelerationPerSecond { get; set; } = 0.5f;
-        private float MaxSpeed { get; set; }
-
-        private float Radius { get; set; } = 0.15f;
-
-        private float sensorRange { get; set; } = 5f;
-
+        private readonly float AccelerationPerSecond;
+        private readonly float DeccelerationPerSecond;
+        private readonly float MaxSpeed;
+        private readonly float Radius = 0.15f;
+        private readonly float sensorRange;
         private readonly float originalLeftVel, originalRightVel;
+
 
         public Vector3 Position { get; private set; }
         public float Rotation { get; private set; }
@@ -101,10 +96,12 @@ public partial class SimulationProvider : Node
 
         public void Update(int leftInput, int rightInput)
         {
-            const float STEPSIZE = 0.2f;
+            const float STEPSIZE = 0.01f;
             // Compute acceleration amounts
             float leftAcc = 0;
             float rightAcc = 0;
+
+            //Console.WriteLine(leftInput); Console.WriteLine(rightInput);
 
             leftAcc += AccelerationPerSecond * STEPSIZE * leftInput;
             rightAcc += AccelerationPerSecond * STEPSIZE * rightInput;
@@ -120,16 +117,14 @@ public partial class SimulationProvider : Node
             else
                 RightVel = Math.Clamp(rightAcc + RightVel, -MaxSpeed, MaxSpeed);
 
-
             float rotAmount = (RightVel - LeftVel) / (Radius * 2);
 
             Rotation += rotAmount * STEPSIZE;
 
             float velocity = (LeftVel + RightVel) / 2;
-
             Vector3 movementVector = (Vector3.Forward * velocity).Rotated(new Vector3(0, 1, 0), Rotation);
 
-            Position += movementVector;
+            Position += movementVector * STEPSIZE;
         }
 
         public int ComputeReward()
@@ -173,14 +168,21 @@ public partial class SimulationProvider : Node
             const float PRECISION = 0.05f;
 
             Vector2 step = new Vector2(target.X - origin.X, target.Z - origin.Z) * PRECISION;
-            Vector2 halfMapSize = occupancyMap.MapSize / 2;
 
+            float lengthStep = step.Length();
+
+            Vector2 halfMapSize = occupancyMap.MapSize / 2;
             Vector2 current = new Vector2(origin.X, origin.Z) + halfMapSize;
 
             CellLite cell = new CellLite { X = -5000, Y = -5000 };
             int rewardCount = 0;
 
-            for (float i = 0; i <= 1; i += PRECISION)
+            float length = 0;
+
+            float occMapHeight = occupancyMap.CellContents.GetLength(0);
+            float occMapWidth = occupancyMap.CellContents.GetLength(1);
+
+            for (float i = 0; i <= 1; i += PRECISION, length += lengthStep)
             {
                 CellLite nextCell = new()
                 {
@@ -188,22 +190,25 @@ public partial class SimulationProvider : Node
                     Y = (int)Math.Round(current.Y / occupancyMap.CellSize.Y)
                 };
 
-                if (nextCell.Y < 0 || nextCell.Y >= occupancyMap.CellContents.GetLength(0))
-                    break;
+                bool OOB = nextCell.Y < 0 || nextCell.Y >= occMapHeight || nextCell.X < 0 || nextCell.X >= occMapWidth;
 
-                if (nextCell.X < 0 || nextCell.X >= occupancyMap.CellContents.GetLength(1))
-                    break;
+                if (OOB)
+                {
+                    if (length < Radius * 1.5f)
+                        rewardCount = -100000;
 
+                    break;
+                }
                 ref var actualCell = ref occupancyMap.CellContents[nextCell.Y, nextCell.X];
 
                 // Let's not give more info
                 if (actualCell.OccupiedLikelihood > 0.5f)
                 {
-                    if ((step * i).Length() <= Radius * 1.5f)
+                    if (length <= Radius * 1.5f)
                     {
                         rewardCount = -50000;
+                        break;
                     }
-                    break;
                 }
 
                 current += step;
@@ -217,7 +222,7 @@ public partial class SimulationProvider : Node
                 cell = nextCell;
             }
 
-            SensorValues[index] = (current - new Vector2(origin.X, origin.Z) + halfMapSize).Length();
+            SensorValues[index] = length;
 
             return rewardCount;
         }
